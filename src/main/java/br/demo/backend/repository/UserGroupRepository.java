552702f86e082a47a32e27725d70187b1e0ef5ee
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-31 21:21:07 -0300

    Atualização de ralação

--- src/main/java/br/demo/backend/repository/UserGroupRepository.java
+++ src/main/java/br/demo/backend/repository/UserGroupRepository.java
@@ -1,8 +1,9 @@
 package br.demo.backend.repository;
 
+import br.demo.backend.model.UserGroupModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProjectModel extends JpaRepository<UserProjectModel, Long> {
+public interface UserGroupRepository extends JpaRepository<UserGroupModel, Long> {
 }
