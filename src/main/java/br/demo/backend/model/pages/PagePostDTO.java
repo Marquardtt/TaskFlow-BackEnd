242bae87c81464fc76d5db5a1ec4cda4f17b27cd
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-11-29 21:04:39 -0300

    Page regras de negocio

--- src/main/java/br/demo/backend/model/pages/PagePostDTO.java
+++ src/main/java/br/demo/backend/model/pages/PagePostDTO.java
@@ -1,11 +1,9 @@
 package br.demo.backend.model.pages;
 
-
 import br.demo.backend.model.Project;
+import br.demo.backend.model.enums.TypeOfPage;
 import br.demo.backend.model.properties.Property;
 import br.demo.backend.model.relations.TaskPage;
-import br.demo.backend.model.tasks.Task;
-import br.demo.backend.model.enums.TypeOfPage;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,24 +11,13 @@ import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "tb_page")
+public class PagePostDTO {
 
-public class Page {
-    @Id
-    private Long id;
     private String name;
-    @Enumerated(value = EnumType.STRING)
     private TypeOfPage type ;
-    @ManyToOne
     private Project project;
-    @OneToMany
-    private Collection<TaskPage> tasks;
-    @ManyToMany
-    private Collection<Property> properties;
-    @ManyToOne
-    private Property propertyOrdering;
+
 }
--- src/main/java/br/demo/backend/service/pages/PageService.java
+++ src/main/java/br/demo/backend/service/pages/PageService.java
@@ -2,17 +2,25 @@ package br.demo.backend.service.pages;
 
 
 import br.demo.backend.model.pages.Page;
+import br.demo.backend.model.pages.PagePostDTO;
+import br.demo.backend.model.properties.Property;
+import br.demo.backend.repository.ProjectRepository;
 import br.demo.backend.repository.pages.PageRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Properties;
 
 @Service
 @AllArgsConstructor
 public class PageService {
 
     PageRepository pageRepository;
+    ProjectRepository projectRepository;
 
     public Collection<Page> findAll() {
         return pageRepository.findAll();
@@ -22,7 +30,14 @@ public class PageService {
         return pageRepository.findById(id).get();
     }
 
-    public void save(Page page) {
+    public void save(PagePostDTO pagePostDTO) {
+        ArrayList<Property> properties = new ArrayList<>(pagePostDTO.getProject().getProperties());
+        Page page = new Page();
+        BeanUtils.copyProperties(pagePostDTO, page);
+        page.setPropertyOrdering(properties.get(0));
+        pageRepository.save(page);
+    }
+    public void update(Page page) {
         pageRepository.save(page);
     }
 
