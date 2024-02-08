f0d4024d9f9df598a4c04bce9a86a3c1bc1b8b1b
Jonatas Jackson Goncalves <jonatas_goncalves@estudante.sesisenai.org.br>
2024-02-08 14:11:14 -0300

    Model Mapper COonfigurado

---
+++ src/main/java/br/demo/backend/configuration/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package br.demo.backend.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        modelMapper.getConfiguration().setCollectionsMergeEnabled(true);
+        return modelMapper;
+    }
+}
