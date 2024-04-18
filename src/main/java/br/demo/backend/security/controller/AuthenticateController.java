af01af760d3b81040bfa1ef9a3f9e202485d5d9e
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-04-18 14:17:16 -0300

    chore: refactoring

--- src/main/java/br/demo/backend/security/controller/AuthenticateController.java
+++ src/main/java/br/demo/backend/security/controller/AuthenticateController.java
@@ -15,9 +15,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @AllArgsConstructor
