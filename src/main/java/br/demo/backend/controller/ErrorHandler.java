45a1646790cf4af6db4cefe218482068e5a38a82
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-04-21 15:56:14 -0300

    error handling aplicado

--- src/main/java/br/demo/backend/controller/ErrorHandler.java
+++ src/main/java/br/demo/backend/controller/ErrorHandler.java
@@ -1,9 +1,16 @@
 package br.demo.backend.controller;
 
+import br.demo.backend.exception.*;
 import br.demo.backend.service.ErrorLoggerService;
+import br.demo.backend.utils.IdProjectValidation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
 
 @ControllerAdvice
 @AllArgsConstructor
@@ -11,9 +18,86 @@ public class ErrorHandler {
 
     private final ErrorLoggerService exceptionLogger;
 
-//    @ExceptionHandler(Exception.class)
-    public void handleException(Exception exception) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+    exceptionLogger.logException(exception);
+    return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException exception) {
         exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleException(NoSuchElementException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleException(NullPointerException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AlreadyInGroupException.class)
+    public ResponseEntity<String> handleException(AlreadyInGroupException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(GroupNotFoundException.class)
+    public ResponseEntity<String> handleException(GroupNotFoundException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HeHaveGroupsException.class)
+    public ResponseEntity<String> handleException(HeHaveGroupsException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HeHaveProjectsException.class)
+    public ResponseEntity<String> handleException(HeHaveProjectsException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(IdGroupDontMatchException.class)
+    public ResponseEntity<String> handleException(IdGroupDontMatchException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(IdProjectDontMatchException.class)
+    public ResponseEntity<String> handleException(IdProjectDontMatchException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(PropertyCantBeDeletedException.class)
+    public ResponseEntity<String> handleException(PropertyCantBeDeletedException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(SomeUserAlreadyIsInProjectException.class)
+    public ResponseEntity<String> handleException(SomeUserAlreadyIsInProjectException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(TaskAlreadyCompleteException.class)
+    public ResponseEntity<String> handleException(TaskAlreadyCompleteException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(TaskAlreadyDeletedException.class)
+    public ResponseEntity<String> handleException(TaskAlreadyDeletedException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(UserCantBeAddedInThisGroupException.class)
+    public ResponseEntity<String> handleException(UserCantBeAddedInThisGroupException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(UsernameAlreadyUsedException.class)
+    public ResponseEntity<String> handleException(UsernameAlreadyUsedException exception) {
+        exceptionLogger.logException(exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
